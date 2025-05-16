8f0cb38b9a9694ac8e524b319ed1e6b844d45ea7
kangsb-010313 <kangsb668@naver.com>
2025-05-16 17:30:23 +0900

    ps 250516 17:30

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -1,13 +1,23 @@
 package echo;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		//소켓 생성 종이컵전화기
+		Socket socket = new Socket();
+		
+		//클라이언트 시작
+		System.out.println("클라이언트 시작>");
+		System.out.println("=================================");
 		
 		//서버에 연결 요청
-		
+		System.out.println("[서버에 연결을 요청합니다.]");
+		socket.connect(new InetSocketAddress("192.168.1.42", 10001));
 		//쓰기 스트림 준비
 		
 		//읽기 스트림 준비
@@ -24,9 +34,11 @@ public class Client {
 		
 		
 		//클라이언트 종료
-		
+		System.out.println("================================");
+		System.out.println("<클라이언트 종료>");
 		
 		//자원 정리
+		socket.close();
 	}
 	
 }
--- src/echo/Server.java
+++ src/echo/Server.java
@@ -1,16 +1,27 @@
 package echo;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 
 		//서버 소켓 생성
+		ServerSocket serverSocket = new ServerSocket();
 		
 		//바인드
-		
+		serverSocket.bind(new InetSocketAddress("192.168.1.42", 10001));
+	
 		//서버 시작
+		System.out.println("<서버 시작>");
+		System.out.println("==========================");
+		System.out.println("[연결 대기 중]");
 		
 		//socket 종이컵전화기
+		Socket socket = serverSocket.accept();
 		
 		//읽기 스트림 준비
 		
@@ -20,8 +31,15 @@ public class Server {
 		//메시지 받기
 		//메시지 보내기
 		
+		//서버 종료
+		System.out.println("==========================");
+		System.out.println("<서버 종료>");
 		
 		//자원 정리
+		
+		socket.close();
+		serverSocket.close();
+		
 	}
 	
 	
