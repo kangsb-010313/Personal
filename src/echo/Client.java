a6a53f7aa99d3ad5770988f4b26093aeaf133d64
kangsb-010313 <kangsb668@naver.com>
2025-05-16 22:19:00 +0900

    ps 240516 22:18

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -1,8 +1,15 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -17,27 +24,42 @@ public class Client {
 		
 		//서버에 연결 요청
 		System.out.println("[서버에 연결을 요청합니다.]");
-		socket.connect(new InetSocketAddress("192.168.1.42", 10001));
+		socket.connect(new InetSocketAddress("192.0.0.2", 10001));
+		
 		//쓰기 스트림 준비
+		OutputStream out = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
 		
 		//읽기 스트림 준비
+		InputStream in = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
 		
 		//스캐너 준비
-		
+		Scanner sc = new Scanner(System.in);
 		
 		//반복
 		//메세지 키보드로 입력받기
+		String msg = sc.nextLine();
 		
 		//메세지 보내기
+		bw.write(msg);
+		bw.newLine();
+		bw.flush();
 		
 		//메세지 받기
-		
+		String reMsg = br.readLine();
+		System.out.println("server:[" + reMsg + "]");
 		
 		//클라이언트 종료
 		System.out.println("================================");
 		System.out.println("<클라이언트 종료>");
 		
 		//자원 정리
+		sc.close();
+		br.close();
+		bw.close();
 		socket.close();
 	}
 	
--- src/echo/Server.java
+++ src/echo/Server.java
@@ -1,6 +1,12 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,30 +19,46 @@ public class Server {
 		ServerSocket serverSocket = new ServerSocket();
 		
 		//바인드
-		serverSocket.bind(new InetSocketAddress("192.168.1.42", 10001));
+		serverSocket.bind(new InetSocketAddress("192.0.0.2", 10001));
 	
 		//서버 시작
 		System.out.println("<서버 시작>");
 		System.out.println("==========================");
-		System.out.println("[연결 대기 중]");
+		System.out.println("[연결을 기다리고 있습니다.]");
 		
 		//socket 종이컵전화기
 		Socket socket = serverSocket.accept();
+		System.out.println("클라이언트가 연결되었습니다.");
 		
 		//읽기 스트림 준비
+		InputStream in = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
 		
 		//쓰기 스트림 준비
+		OutputStream out = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
 		
 		//반복
 		//메시지 받기
+		String msg = br.readLine();
+		
+		System.out.println("받은메세지: " + msg);
+		
 		//메시지 보내기
+		bw.write(msg);
+		bw.newLine();
+		bw.flush();
+		
 		
 		//서버 종료
 		System.out.println("==========================");
 		System.out.println("<서버 종료>");
 		
 		//자원 정리
-		
+		bw.close();
+		br.close();
 		socket.close();
 		serverSocket.close();
 		
