ed95588a5ef85e26e3bf824f46e4eae620e173e9
kangsb-010313 <kangsb668@naver.com>
2025-05-16 16:41:28 +0900

    ps 250516 16:41 socket

---
+++ src/echo/Server.java
@@ -0,0 +1,28 @@
+package echo;
+
+public class Server {
+
+	public static void main(String[] args) {
+
+		//서버 소켓 생성
+		
+		//바인드
+		
+		//서버 시작
+		
+		//socket 종이컵전화기
+		
+		//읽기 스트림 준비
+		
+		//쓰기 스트림 준비
+		
+		//반복
+		//메시지 받기
+		//메시지 보내기
+		
+		
+		//자원 정리
+	}
+	
+	
+}
